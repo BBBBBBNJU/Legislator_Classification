da62e43323e846ea7ab7e54d52a3ffc6a15605b6
Hao Yan <haoyan@wustl.edu>
2017-05-08 11:41:14 -0500

    add ks score

---
+++ add_KS.py
@@ -0,0 +1,42 @@
+import csv
+import os
+import sys
+
+def loadData(fileName):
+    reader = csv.reader(file(fileName, 'rb'))
+    i=0
+    allData=[]
+    for line in reader:
+        i+=1
+        if i==1:
+            catagory=line
+        else:
+            allData.append(line)
+    return catagory,allData
+folder = sys.argv[1]
+peopleInforFile = folder + '/congress_people_info.csv'
+catagory, allData = loadData(peopleInforFile)
+ksScoreDict = {}
+for eachdata in allData:
+    name = eachdata[0]
+    score = eachdata[-1]
+    ksScoreDict[name] = score
+newFile = open(folder + '/new_non-overlap.csv','w')
+
+originalFile = folder + '/non-overlap.csv'
+catagory, allData = loadData(originalFile)
+catagory += ['K-S score']
+newFile.write(','.join(catagory) + '\n')
+
+for eachData in allData:
+    tempname = eachData[0]
+    tempScore = 'NaN'
+    for key, value in ksScoreDict.iteritems():
+        if key in tempname:
+            tempScore = value
+            break
+    newVec = eachData + [tempScore]
+    newFile.write(','.join(newVec) + '\n')
+
+newFile.close()
+    
